6eeb70757f615e1cf0e9300d21a38e92f7bb13f1
Cui <rn597959@dal.ca>
2018-01-13 16:47:34 -0400

    create

---
+++ src/main/java/com/cui/csci2110/App.java
@@ -0,0 +1,13 @@
+package com.cui.csci2110;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
